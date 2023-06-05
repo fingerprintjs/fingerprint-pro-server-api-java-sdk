ac085cee7b8f0934d83476071bee28a554b8676b
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-06-05 14:44:57 +0200

    fix: fix release workflow for generate.sh again

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,7 +5,7 @@ VERSION='dev-2.0.0-2'
 while getopts "v:" arg; do
   case $arg in
     v)
-      VERSION='dev-2.0.0-2'
+      VERSION=$OPTARG
       ;;
   esac
 done
@@ -13,16 +13,16 @@ done
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
 if [[ $VERSION == *"develop"* ]]; then
-  SANITIZED_VERSION='dev-2.0.0-2'
-  SANITIZED_VERSION='dev-2.0.0-2'
-  BUILD_VERSION='dev-2.0.0-2'
-  VERSION='dev-2.0.0-2'
+  SANITIZED_VERSION=$(sed 's/-develop//g' <<< $VERSION)
+  SANITIZED_VERSION=$(sed 's/\.[0-9]*$//g' <<< $SANITIZED_VERSION)
+  BUILD_VERSION=$(grep -o '[0-9]*$' <<< $VERSION)
+  VERSION="dev-$SANITIZED_VERSION-$BUILD_VERSION"
 fi
 
 echo "VERSION: $VERSION"
 
 sed -i "s/artifactVersion: .*/artifactVersion: $VERSION/g" config.yaml
-sed -i "s/VERSION='dev-2.0.0-2'
+sed -i "s/^VERSION=.*/VERSION='$VERSION'/g" ./scripts/generate.sh
 
 rm -rf pom.xml README.md build.gradle settings.gradle gradle.properties
 

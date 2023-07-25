d386f5d7fee8e558eefe39ce26c94d73a77370e0
Juraj Uhlar <juro.uhlar@gmail.com>
2023-07-25 11:22:15 +0100

    chore(functional-tests): load env variables, print results

--- src/examples/java/com/fingerprint/example/FunctionalTests.java
+++ src/examples/java/com/fingerprint/example/FunctionalTests.java
@@ -5,36 +5,47 @@ import com.fingerprint.sdk.ApiClient;
 import com.fingerprint.sdk.ApiException;
 import com.fingerprint.sdk.Configuration;
 import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
 
 public class FunctionalTests {
     public static void main(String... args) {
-        Dotenv dotenv = Dotenv.configure().load();
+
+        // Load variables from .env if present, host environment variables still take precedence if present
+        File envFile = new File(".env");
+        Dotenv dotenv = null;
+        if (envFile.exists()) {
+            dotenv = Dotenv.configure().load();
+        } else {
+            System.out.println(".env file not found. Skipping dotenv loading.");
+        }
 
         String FPJS_API_SECRET = System.getenv("FPJS_API_SECRET");
-        if (FPJS_API_SECRET == null) {
+        if (FPJS_API_SECRET == null && dotenv != null) {
             FPJS_API_SECRET = dotenv.get("FPJS_API_SECRET");
         }
 
         String FPJS_VISITOR_ID = System.getenv("FPJS_VISITOR_ID");
-        if (FPJS_VISITOR_ID == null) {
+        if (FPJS_VISITOR_ID == null && dotenv != null) {
             FPJS_VISITOR_ID = dotenv.get("FPJS_VISITOR_ID");
         }
 
         String FPJS_REQUEST_ID = System.getenv("FPJS_REQUEST_ID");
-        if (FPJS_REQUEST_ID == null) {
+        if (FPJS_REQUEST_ID == null && dotenv != null) {
             FPJS_REQUEST_ID = dotenv.get("FPJS_REQUEST_ID");
         }
 
         String FPJS_API_REGION = System.getenv("FPJS_API_REGION");
-        if (FPJS_API_REGION == null) {
+        if (FPJS_API_REGION == null && dotenv != null) {
             FPJS_API_REGION = dotenv.get("FPJS_API_REGION");
         }
 
-      
 
+        // Create a new instance of the API client
         ApiClient client = Configuration.getDefaultApiClient(FPJS_API_SECRET, FPJS_API_REGION != null ? FPJS_API_REGION : "eu");
         FingerprintApi api = new FingerprintApi(client);
 
+
+        // Get identification event
         try {
             final Object event = api.getEvent(FPJS_REQUEST_ID);
             System.out.println(event);
@@ -43,6 +54,7 @@ public class FunctionalTests {
             System.exit(1);
         }
 
+        // Get visitor history
         try {
             final Object visits = api.getVisits(FPJS_VISITOR_ID, null, null, null, null, null);
             System.out.println(visits);

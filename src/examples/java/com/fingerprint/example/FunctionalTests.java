588feb660d1301c884ad32156b1f6680e488da15
Juraj Uhlar <juro.uhlar@gmail.com>
2023-07-25 10:51:34 +0100

    chore(functional-tests): functional tests load env vars, print results

--- src/examples/java/com/fingerprint/example/FunctionalTests.java
+++ src/examples/java/com/fingerprint/example/FunctionalTests.java
@@ -4,26 +4,48 @@ import com.fingerprint.api.FingerprintApi;
 import com.fingerprint.sdk.ApiClient;
 import com.fingerprint.sdk.ApiException;
 import com.fingerprint.sdk.Configuration;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class FunctionalTests {
     public static void main(String... args) {
-        final String FPJS_API_SECRET = System.getenv("FPJS_API_SECRET");
-        final String FPJS_VISITOR_ID = System.getenv("FPJS_VISITOR_ID");
-        final String FPJS_REQUEST_ID = System.getenv("FPJS_REQUEST_ID");
-        final String FPJS_API_REGION = System.getenv("FPJS_API_REGION");
+        Dotenv dotenv = Dotenv.configure().load();
+
+        String FPJS_API_SECRET = System.getenv("FPJS_API_SECRET");
+        if (FPJS_API_SECRET == null) {
+            FPJS_API_SECRET = dotenv.get("FPJS_API_SECRET");
+        }
+
+        String FPJS_VISITOR_ID = System.getenv("FPJS_VISITOR_ID");
+        if (FPJS_VISITOR_ID == null) {
+            FPJS_VISITOR_ID = dotenv.get("FPJS_VISITOR_ID");
+        }
+
+        String FPJS_REQUEST_ID = System.getenv("FPJS_REQUEST_ID");
+        if (FPJS_REQUEST_ID == null) {
+            FPJS_REQUEST_ID = dotenv.get("FPJS_REQUEST_ID");
+        }
+
+        String FPJS_API_REGION = System.getenv("FPJS_API_REGION");
+        if (FPJS_API_REGION == null) {
+            FPJS_API_REGION = dotenv.get("FPJS_API_REGION");
+        }
+
+      
 
         ApiClient client = Configuration.getDefaultApiClient(FPJS_API_SECRET, FPJS_API_REGION != null ? FPJS_API_REGION : "eu");
         FingerprintApi api = new FingerprintApi(client);
 
         try {
-            api.getEvent(FPJS_REQUEST_ID);
+            final Object event = api.getEvent(FPJS_REQUEST_ID);
+            System.out.println(event);
         } catch (ApiException e) {
             System.err.println("Exception when calling FingerprintApi.getEvent:" + e.getMessage());
             System.exit(1);
         }
 
         try {
-            api.getVisits(FPJS_VISITOR_ID, null, null, null, null, null);
+            final Object visits = api.getVisits(FPJS_VISITOR_ID, null, null, null, null, null);
+            System.out.println(visits);
         } catch (ApiException e) {
             System.err.println("Exception when calling FingerprintApi.getEvent:" + e.getMessage());
             System.exit(1);

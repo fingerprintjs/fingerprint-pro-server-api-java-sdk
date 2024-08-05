97afe9057b08a641d8a5bb17a638b26c34ba47cb
Sergey Shelomentsev <sergey.shelomentsev@fingerprint.com>
2024-08-05 18:51:41 +0300

    chore: add webhook sample
    chore: set encoding
    
    chore: add webhook sample to readme

--- sdk/src/test/java/com/fingerprint/WebhookTest.java
+++ sdk/src/test/java/com/fingerprint/WebhookTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,7 +13,7 @@ public class WebhookTest {
 
     private static final String validHeader = "v1=1b2c16b75bd2a870c114153ccda5bcfca63314bc722fa160d690de133ccbb9db";
     private static final String secret = "secret";
-    private static final byte[] data = "data".getBytes();
+    private static final byte[] data = "data".getBytes(StandardCharsets.UTF_8);
 
     @Test
     public void validHeaderTest() throws NoSuchAlgorithmException {

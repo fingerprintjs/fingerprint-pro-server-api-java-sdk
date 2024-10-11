22aa38705331ff86c61076e1c8bf6daf3f75e269
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-10-11 10:31:47 +0200

    build: don't modify pre-release versions

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,13 +10,6 @@ while getopts "v:" arg; do
   esac
 done
 
-if [[ $VERSION == *"develop"* ]]; then
-  SANITIZED_VERSION=$(sed 's/-develop//g' <<< $VERSION)
-  SANITIZED_VERSION=$(sed 's/\.[0-9]*$//g' <<< $SANITIZED_VERSION)
-  BUILD_VERSION=$(grep -o '[0-9]*$' <<< $VERSION)
-  VERSION="dev-$SANITIZED_VERSION-$BUILD_VERSION"
-fi
-
 echo "VERSION: $VERSION"
 
 platform=$(uname)

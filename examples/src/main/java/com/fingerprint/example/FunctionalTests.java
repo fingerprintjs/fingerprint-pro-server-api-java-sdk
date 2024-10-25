fd779d90b28de88c0ab81d2adf4107db1b2b9e4f
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2024-10-25 13:12:13 +0200

    test: fix types and mocks paths for tests after schema update

--- examples/src/main/java/com/fingerprint/example/FunctionalTests.java
+++ examples/src/main/java/com/fingerprint/example/FunctionalTests.java
@@ -1,9 +1,14 @@
 package com.fingerprint.example;
 
 import com.fingerprint.api.FingerprintApi;
+import com.fingerprint.model.EventsUpdateRequest;
 import com.fingerprint.sdk.ApiClient;
 import com.fingerprint.sdk.ApiException;
 import com.fingerprint.sdk.Configuration;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
 
 public class FunctionalTests {
     public static void main(String... args) {
@@ -11,6 +16,8 @@ public class FunctionalTests {
         String FPJS_VISITOR_ID = System.getenv("FPJS_VISITOR_ID");
         String FPJS_REQUEST_ID = System.getenv("FPJS_REQUEST_ID");
         String FPJS_API_REGION = System.getenv("FPJS_API_REGION");
+        String FPJS_REQUEST_ID_TO_UPDATE = System.getenv("FPJS_REQUEST_ID_TO_UPDATE");
+        String FPJS_VISITOR_ID_TO_DELETE = System.getenv("FPJS_VISITOR_ID_TO_DELETE");
 
         // Create a new instance of the API client
         ApiClient client = Configuration.getDefaultApiClient(FPJS_API_SECRET, FPJS_API_REGION != null ? FPJS_API_REGION : "us");
@@ -33,6 +40,31 @@ public class FunctionalTests {
         } catch (ApiException e) {
             System.err.println("Exception when calling FingerprintApi.getEvent:" + e.getMessage());
             System.exit(1);
+        }
+
+        // Update identification event
+
+        if (FPJS_REQUEST_ID_TO_UPDATE != null) {
+            try {
+                final HashMap<String, Object> tags = new HashMap() {{
+                    put("timestamp", new Timestamp(new Date().getTime()));
+                }};
+                api.updateEvent(FPJS_REQUEST_ID_TO_UPDATE, new EventsUpdateRequest().tag(tags));
+            } catch (ApiException e) {
+                System.err.println("Exception when calling FingerprintApi.updateEvent:" + e.getMessage());
+                System.exit(1);
+            }
+        }
+
+        // Delete visitor data
+
+        if (FPJS_VISITOR_ID_TO_DELETE != null) {
+            try {
+                api.deleteVisitorData(FPJS_VISITOR_ID_TO_DELETE);
+            } catch (ApiException e) {
+                System.err.println("Exception when calling FingerprintApi.deleteVisitor:" + e.getMessage());
+                System.exit(1);
+            }
         }
 
         System.out.println("Checks Passed");
--- sdk/src/test/java/com/fingerprint/SerializationTest.java
+++ sdk/src/test/java/com/fingerprint/SerializationTest.java
@@ -3,9 +3,8 @@ package com.fingerprint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fingerprint.model.EventResponse;
-import com.fingerprint.model.RawDeviceAttributesResultValue;
-import com.fingerprint.model.SignalResponseRawDeviceAttributes;
+import com.fingerprint.model.EventsGetResponse;
+import com.fingerprint.model.ProductRawDeviceAttributes;
 import com.fingerprint.sdk.ApiException;
 import com.fingerprint.sdk.JSON;
 import org.junit.jupiter.api.Test;
@@ -42,15 +41,15 @@ public class SerializationTest {
     @Test
     public void serializeRawDeviceAttributesTest() throws IOException {
         ObjectMapper sdkObjectMapper = JSON.getDefault().getMapper();
-        EventResponse eventResponse = sdkObjectMapper.readValue(getFileAsIOStream("mocks/get_event_200.json"), EventResponse.class);
+        EventsGetResponse eventResponse = sdkObjectMapper.readValue(getFileAsIOStream("mocks/get_event_200.json"), EventsGetResponse.class);
 
-        SignalResponseRawDeviceAttributes signalResponseRawDeviceAttributes = eventResponse.getProducts().getRawDeviceAttributes();
+        ProductRawDeviceAttributes signalResponseRawDeviceAttributes = eventResponse.getProducts().getRawDeviceAttributes();
         String sdkResult = sdkObjectMapper.writeValueAsString(signalResponseRawDeviceAttributes);
 
         ObjectMapper springLikeObjectMapper = getMapper();
         String springResult = springLikeObjectMapper.writeValueAsString(signalResponseRawDeviceAttributes);
 
         assertTrue(sdkResult.contains("\"architecture\":{\"value\":127}"));
-        assertTrue(springResult.contains("\"architecture\":{\"error\":null,\"value\":127}"));
+        assertTrue(springResult.contains("\"architecture\":{\"value\":127,\"error\":null}"));
     }
 }

cfacdcfa6c8752f08b9974295da3f423ca1f285b
TheUnderScorer <TheUnderScorer@users.noreply.github.com>
2025-02-24 14:44:17 +0000

    feat: update OpenAPI schema

--- sdk/src/main/java/com/fingerprint/api/FingerprintApi.java
+++ sdk/src/main/java/com/fingerprint/api/FingerprintApi.java
@@ -385,13 +385,13 @@ public class FingerprintApi {
    * Search for identification events, including Smart Signals, using multiple filtering criteria. If you don't provide `start` or `end` parameters, the default search range is the last 7 days.  Please note that events include mobile signals (e.g. `rootApps`) even if the request originated from a non-mobile platform. We recommend you **ignore** mobile signals for such requests. 
    * @param limit Limit the number of events returned.  (required)
    * @param visitorId Unique [visitor identifier](https://dev.fingerprint.com/reference/get-function#visitorid) issued by Fingerprint Pro. Filter for events matching this `visitor_id`.  (optional)
-   * @param bot Filter events by the bot detection result, specifically:    - events where any kind of bot was detected.   - events where a good bot was detected.   - events where a bad bot was detected.   - events where no bot was detected.  (optional)
+   * @param bot Filter events by the bot detection result, specifically:    `all` - events where any kind of bot was detected.   `good` - events where a good bot was detected.   `bad` - events where a bad bot was detected.   `none` - events where no bot was detected.  (optional)
    * @param ipAddress Filter events by IP address range. The range can be as specific as a single IP (/32 for IPv4 or /128 for IPv6)  All ip_address filters must use CIDR notation, for example, 10.0.0.0/24, 192.168.0.1/32  (optional)
    * @param linkedId Filter events by your custom identifier.   You can use [linked IDs](https://dev.fingerprint.com/reference/get-function#linkedid) to associate identification requests with your own identifier, for example, session ID, purchase ID, or transaction ID. You can then use this `linked_id` parameter to retrieve all events associated with your custom identifier.  (optional)
    * @param start Filter events with a timestamp greater than the start time, in Unix time (milliseconds).  (optional)
    * @param end Filter events with a timestamp smaller than the end time, in Unix time (milliseconds).  (optional)
    * @param reverse Sort events in reverse timestamp order.  (optional)
-   * @param suspect Filter events previously tagged as suspicious via the [Update API](https://dev.fingerprint.com/reference/updateevent).  (optional)
+   * @param suspect Filter events previously tagged as suspicious via the [Update API](https://dev.fingerprint.com/reference/updateevent).  > Note: When using this parameter, only events with the `suspect` property explicitly set to `true` or `false` are returned. Events with undefined `suspect` property are left out of the response.  (optional)
    * @return SearchEventsResponse
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -411,13 +411,13 @@ public class FingerprintApi {
    * Search for identification events, including Smart Signals, using multiple filtering criteria. If you don't provide `start` or `end` parameters, the default search range is the last 7 days.  Please note that events include mobile signals (e.g. `rootApps`) even if the request originated from a non-mobile platform. We recommend you **ignore** mobile signals for such requests. 
    * @param limit Limit the number of events returned.  (required)
    * @param visitorId Unique [visitor identifier](https://dev.fingerprint.com/reference/get-function#visitorid) issued by Fingerprint Pro. Filter for events matching this `visitor_id`.  (optional)
-   * @param bot Filter events by the bot detection result, specifically:    - events where any kind of bot was detected.   - events where a good bot was detected.   - events where a bad bot was detected.   - events where no bot was detected.  (optional)
+   * @param bot Filter events by the bot detection result, specifically:    `all` - events where any kind of bot was detected.   `good` - events where a good bot was detected.   `bad` - events where a bad bot was detected.   `none` - events where no bot was detected.  (optional)
    * @param ipAddress Filter events by IP address range. The range can be as specific as a single IP (/32 for IPv4 or /128 for IPv6)  All ip_address filters must use CIDR notation, for example, 10.0.0.0/24, 192.168.0.1/32  (optional)
    * @param linkedId Filter events by your custom identifier.   You can use [linked IDs](https://dev.fingerprint.com/reference/get-function#linkedid) to associate identification requests with your own identifier, for example, session ID, purchase ID, or transaction ID. You can then use this `linked_id` parameter to retrieve all events associated with your custom identifier.  (optional)
    * @param start Filter events with a timestamp greater than the start time, in Unix time (milliseconds).  (optional)
    * @param end Filter events with a timestamp smaller than the end time, in Unix time (milliseconds).  (optional)
    * @param reverse Sort events in reverse timestamp order.  (optional)
-   * @param suspect Filter events previously tagged as suspicious via the [Update API](https://dev.fingerprint.com/reference/updateevent).  (optional)
+   * @param suspect Filter events previously tagged as suspicious via the [Update API](https://dev.fingerprint.com/reference/updateevent).  > Note: When using this parameter, only events with the `suspect` property explicitly set to `true` or `false` are returned. Events with undefined `suspect` property are left out of the response.  (optional)
    * @return ApiResponse&lt;SearchEventsResponse&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details

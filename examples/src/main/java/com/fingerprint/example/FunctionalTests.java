549f81bf09487220f9de1e415993fc19a702b889
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2025-04-01 17:30:11 +0200

    chore: apply review findings

--- examples/src/main/java/com/fingerprint/example/FunctionalTests.java
+++ examples/src/main/java/com/fingerprint/example/FunctionalTests.java
@@ -15,11 +15,11 @@ import java.util.HashMap;
 public class FunctionalTests {
     public static void main(String... args) {
         String FPJS_API_SECRET = System.getenv("FPJS_API_SECRET");
-        String FPJS_VISITOR_ID = "";
-        String FPJS_REQUEST_ID = "";
         String FPJS_API_REGION = System.getenv("FPJS_API_REGION");
         String FPJS_REQUEST_ID_TO_UPDATE = System.getenv("FPJS_REQUEST_ID_TO_UPDATE");
         String FPJS_VISITOR_ID_TO_DELETE = System.getenv("FPJS_VISITOR_ID_TO_DELETE");
+        String FPJS_VISITOR_ID = "";
+        String FPJS_REQUEST_ID = "";
 
         // Create a new instance of the API client
         ApiClient client = Configuration.getDefaultApiClient(FPJS_API_SECRET, FPJS_API_REGION != null ? FPJS_API_REGION : "us");
@@ -34,6 +34,10 @@ public class FunctionalTests {
                     .setStart(start)
                     .setEnd(end)
             );
+            if (events.getEvents().isEmpty()) {
+                System.err.println("FingerprintApi.searchEvents: is empty");
+                System.exit(1);
+            }
             Identification firstEventIdentificationData = events.getEvents().get(0).getProducts().getIdentification().getData();
             FPJS_VISITOR_ID = firstEventIdentificationData.getVisitorId();
             FPJS_REQUEST_ID = firstEventIdentificationData.getRequestId();
@@ -93,9 +97,19 @@ public class FunctionalTests {
                     .setEnd(end)
                     .setReverse(true)
             );
+            if (oldEvents.getEvents().isEmpty()) {
+                System.err.println("FingerprintApi.searchEvents: is empty for old events");
+                System.exit(1);
+            }
             Identification oldEventIdentificationData = oldEvents.getEvents().get(0).getProducts().getIdentification().getData();
             String FPJS_OLD_VISITOR_ID = oldEventIdentificationData.getVisitorId();
             String FPJS_OLD_REQUEST_ID = oldEventIdentificationData.getRequestId();
+
+            if (FPJS_VISITOR_ID.equals(FPJS_OLD_VISITOR_ID) || FPJS_REQUEST_ID.equals(FPJS_OLD_REQUEST_ID)) {
+                System.err.println("Old events are identical to new");
+                System.exit(1);
+            }
+
             api.getEvent(FPJS_OLD_REQUEST_ID);
             api.getVisits(FPJS_OLD_VISITOR_ID, null, null, null, null, null);
             System.out.println("Old events are good");
